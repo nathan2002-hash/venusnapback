d9d93c115cb5e5978744c6cf9caaeddd4d09fc09
Nathan Mwamba <nathanmwamba2002@gmail.com>
2025-02-27 05:39:39 -0800

    first

--- app/Http/Controllers/Api/SupportController.php
+++ app/Http/Controllers/Api/SupportController.php
@@ -5,14 +5,15 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Supporter;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class SupportController extends Controller
 {
     public function support($id)
     {
         $support = new Supporter();
-        $support->supporter_id = '2';
-        $support->supporting_id = $id;
+        $support->user_id = Auth::user()->id;
+        $support->artboard_id = '5';
         $support->status = '1';
         $support->save();
         return redirect()->back();
