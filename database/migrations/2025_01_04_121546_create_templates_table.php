0c42bb9237f831f24c4bf468084bc95ec1b94da2
Nathan Mwamba <nathanmwamba2002@gmail.com>
2025-03-01 10:32:11 -0800

    folk

--- database/migrations/2025_01_04_121546_create_templates_table.php
+++ database/migrations/2025_01_04_121546_create_templates_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('templates', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('type');
+            $table->string('user_id');
+            $table->longText('description');
             $table->string('file_path');
             $table->timestamps();
         });
--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,7 @@ Route::middleware('auth:api')->post('/post/store', 'Api\PostController@store');
 Route::middleware('auth:api')->post('/post/admire', 'Api\PostController@admire');
 Route::middleware('auth:api')->post('/supporter/subscribe', 'Api\SupportController@support');
 Route::middleware('auth:api')->post('/change/password', 'Api\AuthController@changePassword');
+Route::middleware('auth:api')->post('/password/activities', 'Api\AuthController@fetchPasswordActivities');
 
 Route::middleware('auth:api')->get('/categories', function () {
     return response()->json(Category::all());
