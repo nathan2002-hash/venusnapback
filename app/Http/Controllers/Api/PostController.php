03edf850f7ddd4d1e689cc41848103bec63d516e
Nathan Mwamba <nathanmwamba2002@gmail.com>
2025-03-02 10:22:42 -0800

    louy

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -38,7 +38,7 @@ public function index(Request $request)
         $postMediaData = $post->postMedias->map(function ($media) {
             return [
                 'id' => $media->id,
-                'filepath' => Storage::disk('s3')->url($media->file_path),
+                'filepath' => Storage::disk('s3')->url($media->file_path_compress),
                 'sequence_order' => $media->sequence_order,
                 'comments_count' => $media->comments->count(),
                 'likes_count' => $media->admires->count(),
