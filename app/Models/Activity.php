307bc4aee2f1e6237adda1c16ec82af5287e7d01
Nathan Mwamba <nathanmwamba2002@gmail.com>
2025-03-02 08:31:23 -0800

    first

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Activity extends Model
 {
-    //
+    protected $fillable = ['title', 'description', 'source', 'user_id', 'status', 'user_agent'];
 }
