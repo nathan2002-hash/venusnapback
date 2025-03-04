ab53f43450921a4c7f309bd6163eca2ff78e7076
Nathan Mwamba <nathanmwamba2002@gmail.com>
2025-03-04 10:13:14 -0800

    fouy

--- config/logging.php
+++ config/logging.php
@@ -59,11 +59,23 @@ return [
             'ignore_exceptions' => false,
         ],
 
+        // 's3' => [
+        //     'driver' => 's3',
+        //     'level' => 'debug', // or any level you want (info, error, etc.)
+        //     'path' => 'logs/laravel.log', // Path to save logs in S3
+        //     'disk' => 's3',
+        // ],
+
         's3' => [
             'driver' => 's3',
-            'level' => 'debug', // or any level you want (info, error, etc.)
-            'path' => 'logs/laravel.log', // Path to save logs in S3
-            'disk' => 's3',
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_DEFAULT_REGION'),
+            'bucket' => env('AWS_BUCKET'),
+            'url' => env('AWS_URL'),
+            'endpoint' => env('AWS_ENDPOINT'),
+            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+            'throw' => false,
         ],
 
         'single' => [
