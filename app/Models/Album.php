cd75eb7035832b8210b10efc2423965413d05cc9
Nathan Mwamba <nathanmwamba2002@gmail.com>
2025-03-06 09:36:43 -0800

    vin

--- app/Models/Album.php
+++ app/Models/Album.php
@@ -6,7 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Album extends Model
 {
-    protected $fillable = ['name', 'description', 'tags', 'allow_comments', 'enable_rating', 'visibility', 'user_id', 'thumbnail_original', 'business_logo_original', 'cover_image_compressed', 'thumbnail_compressed', 'business_logo_compressed', 'cover_image_original', 'type'];
+    protected $fillable = ['name', 'description', 'tags', 'allow_comments', 'enable_rating', 'visibility',
+      'user_id', 'thumbnail_original', 'business_logo_original', 'cover_image_compressed',
+      'thumbnail_compressed', 'business_logo_compressed', 'cover_image_original', 'type',
+      'phone', 'email', 'location', 'website', 'facebook', 'linkedin', 'is_paid_access', 'business_category'
+    ];
 
     public function user(){
         return $this->belongsTo(User::class);
