e58fe5f4aec8438f02fb5275685e4b598920a7c6
Nathan Mwamba <nathanmwamba2002@gmail.com>
2025-03-07 06:22:20 -0800

    hui

--- app/Models/Artwork.php
+++ app/Models/Artwork.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Artwork extends Model
 {
+    protected $fillable = ['user_id', 'thumbnail'];
+
     public function user(){
         return $this->belongsTo(User::class);
     }
