be57791e47f39e973e1fe7e54e5980a169fee4ec
Nathan Mwamba <nathanmwamba2002@gmail.com>
2025-03-09 23:04:17 -0700

    si

---
+++ database/migrations/2025_03_10_054456_create_user_settings_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_settings', function (Blueprint $table) {
+            $table->id();
+            $table->string('sms_alert')->default(0);
+            $table->string('email_notifications')->default(1);
+            $table->string('tfa')->default(0);
+            $table->string('push_notifications')->default(1);
+            $table->string('dark_mode')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_settings');
+    }
+};
