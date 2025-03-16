719a867342295f881aef242312a3881ee4336427
Nathan Mwamba <nathanmwamba2002@gmail.com>
2025-03-16 15:14:25 -0700

    love

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -53,6 +53,7 @@ class AuthController extends Controller
         $profileUrl = $user->profile_compressed ? Storage::disk('s3')->url($user->profile_compressed) : 'https://www.gravatar.com/avatar/' . md5(strtolower(trim($user->email))) . '?s=100&d=mp';
 
         $preference = ($user->preference === null || $user->preference == 1) ? 1 : 0;
+        $authe = ($user->usersetting->tfa === null || $user->usersetting->tfa == 1) ? 1 : 0;
         // Return the token and user details
         return response()->json([
             'username' => (string) $user->username,
@@ -60,6 +61,7 @@ class AuthController extends Controller
             'token' => $token->accessToken,
             'preference' => (string) $preference,
             'profile' => $profileUrl,
+            '2fa' => (string) $authe,
         ]);
     }
 
