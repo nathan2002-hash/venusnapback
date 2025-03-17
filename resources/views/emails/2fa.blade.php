7557d1e4502961dcea5067078694217b433282e5
Nathan Mwamba <nathanmwamba2002@gmail.com>
2025-03-17 08:42:12 -0700

    push

--- resources/views/emails/2fa.blade.php
+++ resources/views/emails/2fa.blade.php
@@ -3,17 +3,10 @@
 
 Your Two-Factor Authentication (2FA) code is:
 
-# **{{ $code }}**
+<center># **{{ $code }}**</center>
 
 This code is valid for 10 minutes. If you did not request this, please secure your account.
 
-Thanks,
-**{{ config('app.name') }} Team**
-
-<x-mail::button :url="''">
-Button Text
-</x-mail::button>
-
 Thanks,<br>
-{{ config('app.name') }}
+**Venusnap Security Team**
 </x-mail::message>
