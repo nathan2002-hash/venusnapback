00dbe9a1698b41467bd268cfe4eeb09d2107979c
Nathan Mwamba <nathanmwamba2002@gmail.com>
2025-03-22 14:59:18 -0700

    lost

--- app/Http/Controllers/Api/HistoryController.php
+++ app/Http/Controllers/Api/HistoryController.php
@@ -67,7 +67,7 @@ class HistoryController extends Controller
                     'media_count' => $totalMediaCount,
                     'latest_view_date' => $firstView->created_at->format('Y-m-d H:i:s'),
                     'viewed_images' => $views->map(fn($view) => [
-                        'image_url' => $view->postMedia->media_url ?? '',
+                        'image_url' => Storage::disk('s3')->url($view->postMedia->file_path_compress) ?? '',
                         'view_date' => $view->created_at->format('Y-m-d H:i:s'),
                     ])->toArray(),
                 ];
