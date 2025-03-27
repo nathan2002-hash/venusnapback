f82edb41118c56b3e620c4e76fbc01b292a12a9f
Nathan Mwamba <nathanmwamba2002@gmail.com>
2025-03-27 03:02:22 -0700

    liu

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
-    //
+    protected $fillable = ['user_id', 'amount', 'payment_method', 'currency', 'processor', 'payment_no', 'status', 'purpose', 'description'];
 }
