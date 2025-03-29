e9ffdfd3189edc48c65e215ca3c262c88e337e93
Nathan Mwamba <nathanmwamba2002@gmail.com>
2025-03-28 22:21:43 -0700

    lowest

--- app/Models/Payout.php
+++ app/Models/Payout.php
@@ -8,6 +8,10 @@ class Payout extends Model
 {
     protected $fillable = ['user_id', 'amount', 'currency', 'payment_session_id', 'processor', 'status', 'payout_method', 'payout_reason', 'transaction_id', 'requested_at', 'processed_at', 'completed_at', 'failed_at', 'failure_reason'];
 
+    protected $casts = [
+        'requested_at' => 'datetime',
+    ];
+
     public function user(){
         return $this->belongsTo(User::class);
     }
