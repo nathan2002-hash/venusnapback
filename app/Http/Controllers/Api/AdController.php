e1291cc72bd57671b6063e3e549745a573fdf479
nathan2002-hash <78975906+nathan2002-hash@users.noreply.github.com>
2025-04-09 05:47:38 -0700

    Update AdController.php

--- app/Http/Controllers/Api/AdController.php
+++ app/Http/Controllers/Api/AdController.php
@@ -397,7 +397,7 @@ class AdController extends Controller
     ->where('ad_id', $ad->id)
     ->count();
     
-    $conversionRate = ($clicks > 0) ? ($conversions / $clickscount) * 100 : 0;
+    $conversionRate = ($clickscount > 0) ? ($conversions / $clickscount) * 100 : 0;
     
     $total_spent = $ad->adboard->budget - $ad->adboard->points;
 
