7f48494be68a0daf26fd14572f409df3380d3265
nathan2002-hash <78975906+nathan2002-hash@users.noreply.github.com>
2025-04-16 06:36:55 -0700

    Update AlbumAccessController.php

--- app/Http/Controllers/Api/AlbumAccessController.php
+++ app/Http/Controllers/Api/AlbumAccessController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\User;
+use App\Models\Album;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
