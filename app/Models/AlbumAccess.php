837bf75083eaa9f825fe100e34264fef284c1415
nathan2002-hash <78975906+nathan2002-hash@users.noreply.github.com>
2025-04-16 07:57:33 -0700

    Update AlbumAccess.php

--- app/Models/AlbumAccess.php
+++ app/Models/AlbumAccess.php
@@ -13,4 +13,22 @@ class AlbumAccess extends Model
     {
         return $this->belongsTo(User::class, 'user_id');
     }
+
+    // Relationship to the user who is requesting access
+    public function requester()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    // Relationship to the album being requested
+    public function album()
+    {
+        return $this->belongsTo(Album::class);
+    }
+
+    // Relationship to the user who can grant access (album owner)
+    public function granter()
+    {
+        return $this->belongsTo(User::class, 'granted_by');
+    }
 }
