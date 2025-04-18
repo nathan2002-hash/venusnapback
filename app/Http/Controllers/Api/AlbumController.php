742d4ab6a54a555e8c1678fc54e869c656b0740c
nathan2002-hash <78975906+nathan2002-hash@users.noreply.github.com>
2025-04-18 11:00:35 -0700

    Update AlbumController.php

--- app/Http/Controllers/Api/AlbumController.php
+++ app/Http/Controllers/Api/AlbumController.php
@@ -53,7 +53,7 @@ class AlbumController extends Controller
 
     // Owned albums (Eloquent collection)
     $ownedAlbums = Album::where('user_id', $user->id)
-        ->whereIn('type', ['creator', 'business'])
+        ->whereIn('type', ['creator', 'business', 'personal'])
         ->select('id', 'name', 'type')
         ->get();
 
