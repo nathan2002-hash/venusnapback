02eefc15f6bfa82262a0507615fec835bb1fd599
nathan2002-hash <78975906+nathan2002-hash@users.noreply.github.com>
2025-04-19 06:13:38 -0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -147,7 +147,8 @@ Route::middleware(['auth:api', 'check.account.status'])->group(function () {
 
     //ad edit
     Route::get('/adboard/edit/{id}', 'Api\AdController@adboardedit');
-     Route::get('/ad/edit/{id}', 'Api\AdController@editads');
+    Route::get('/ad/edit/{id}', 'Api\AdController@editads');
+    Route::post('/adboard/update/{id}', 'Api\AdController@update');
 
     Route::get('/ad/list', 'Api\AdController@getAds');
     Route::get('/ad/{id}/performance', 'Api\AdController@getAdPerformance');
