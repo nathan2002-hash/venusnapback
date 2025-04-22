52fa709b90889dd8cda78b5b13fdb40936f1b0ee
nathan2002-hash <78975906+nathan2002-hash@users.noreply.github.com>
2025-04-22 02:50:47 -0700

    Update Saved.php

--- app/Models/Saved.php
+++ app/Models/Saved.php
@@ -9,4 +9,8 @@ class Saved extends Model
     public function post(){
         return $this->belongsTo(Post::class);
     }
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
