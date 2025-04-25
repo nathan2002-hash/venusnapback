76eb47fed8c25440ec9b656cb2e95a6440760292
nathan2002-hash <78975906+nathan2002-hash@users.noreply.github.com>
2025-04-25 01:37:56 -0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,7 @@ Route::get('/user', function (Request $request) {
 
 Route::post('/register', 'Api\AuthController@register');
 Route::post('/login', 'Api\AuthController@login');
+Route::post('/forgot/password/code', 'Api\AuthController@sendResetCode');
 
 Route::middleware('auth:api')->post('/logout', function (Request $request) {
     $request->user()->token()->revoke();
