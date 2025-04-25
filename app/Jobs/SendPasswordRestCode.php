12ff45a92bf079fef3d4734dadbe8fbacd65a78d
nathan2002-hash <78975906+nathan2002-hash@users.noreply.github.com>
2025-04-25 01:35:54 -0700

    Update SendPasswordRestCode.php

--- app/Jobs/SendPasswordRestCode.php
+++ app/Jobs/SendPasswordRestCode.php
@@ -4,24 +4,32 @@ namespace App\Jobs;
 
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Support\Facades\Mail;
 
 class SendPasswordRestCode implements ShouldQueue
 {
     use Queueable;
 
+    protected $email;
+    protected $code;
+
     /**
      * Create a new job instance.
      */
-    public function __construct()
+    public function __construct($email, $code)
     {
-        //
+        $this->email = $email;
+        $this->code = $code;
     }
 
     /**
      * Execute the job.
      */
-    public function handle(): void
+    public function handle()
     {
-        //
+        Mail::raw("Your password reset code is: {$this->code}. It expires in 10 minutes.", function ($message) {
+            $message->to($this->email)
+                    ->subject('Password Reset Code');
+        });
     }
 }
