6541c700528bb5b328be44723e658415a0ce16b7
nathan2002-hash <78975906+nathan2002-hash@users.noreply.github.com>
2025-05-01 01:10:45 -0700

    Update PayController.php

--- app/Http/Controllers/Api/PayController.php
+++ app/Http/Controllers/Api/PayController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\PaymentSession;
 use Stripe\Stripe;
+use Stripe\Token;
 use App\Models\Payment;
 use Stripe\PaymentIntent;
 use Illuminate\Support\Facades\Auth;
