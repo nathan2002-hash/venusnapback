07bb1a97e979a0cf5c78c66135a14ade26664bcc
nathan2002-hash <78975906+nathan2002-hash@users.noreply.github.com>
2025-05-06 05:20:02 -0700

    Update AlbumView.php

--- app/Models/AlbumView.php
+++ app/Models/AlbumView.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class AlbumView extends Model
 {
-    protected $fillable = ['user_id', 'album_id', 'ip_address', 'user_agent'];
+    protected $fillable = ['user_id', 'album_id', 'ip_address', 'device_info', 'user_agent'];
 }
