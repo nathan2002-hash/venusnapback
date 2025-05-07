7b233c8087ec470c9c7e89e65bc136ef650b86a8
nathan2002-hash <78975906+nathan2002-hash@users.noreply.github.com>
2025-05-07 02:25:26 -0700

    Update AlbumController.php

--- app/Http/Controllers/Api/AlbumController.php
+++ app/Http/Controllers/Api/AlbumController.php
@@ -54,7 +54,7 @@ class AlbumController extends Controller
     }
 
 
-    public function getUserAlbums()
+    public function getUffserAlbums()
 {
     $user = Auth::user();
 
@@ -98,6 +98,57 @@ class AlbumController extends Controller
                 'album_name' => "{$album->name} ($typeLabel)",
             ];
         }),
+    ]);
+}
+
+    public function getUserAlbums()
+{
+    $user = Auth::user();
+
+    // Owned albums (Eloquent collection)
+    $ownedAlbums = Album::where('user_id', $user->id)
+        ->whereIn('type', ['creator', 'business', 'personal'])
+        ->select('id', 'name', 'type')
+        ->get()
+        ->map(function ($album) {
+            $typeLabel = match($album->type) {
+                'personal' => 'Personal',
+                'creator' => 'Creator',
+                default => 'Business',
+            };
+
+            return [
+                'id' => $album->id,
+                'album_name' => "{$album->name} ($typeLabel)",
+            ];
+        });
+
+    // Shared albums (direct array conversion)
+    $sharedAlbums = DB::table('album_accesses')
+        ->join('albums', 'album_accesses.album_id', '=', 'albums.id')
+        ->where('album_accesses.user_id', $user->id)
+        ->where('album_accesses.status', 'approved')
+        ->whereIn('albums.type', ['creator', 'business'])
+        ->select('albums.id', 'albums.name', 'albums.type')
+        ->get()
+        ->map(function ($album) {
+            $typeLabel = match($album->type) {
+                'creator' => 'Creator',
+                default => 'Business',
+            };
+
+            return [
+                'id' => $album->id,
+                'album_name' => "{$album->name} ($typeLabel)",
+            ];
+        });
+
+    // Merge both collections
+    $albums = $ownedAlbums->merge($sharedAlbums);
+
+    return response()->json([
+        'success' => true,
+        'data' => $albums->values() // This ensures sequential array keys
     ]);
 }
 
