f6aa71c9bc851f9193faa32093274166e33cea03
nathan2002-hash <78975906+nathan2002-hash@users.noreply.github.com>
2025-05-07 07:54:29 -0700

    Update PostExploreController.php

--- app/Http/Controllers/Api/PostExploreController.php
+++ app/Http/Controllers/Api/PostExploreController.php
@@ -132,7 +132,8 @@ class PostExploreController extends Controller
                         'sequence_order' => $media->sequence_order,
                     ];
                 })->toArray(),
-                'is_verified' => $album->is_verified ?? false,
+                //'is_verified' => $album->is_verified ?? false,
+                'is_verified' => (bool) $album->is_verified
                 'supporters_count' => (string) ($album->supporters->count() ?? 0),
                 'is_ad' => false,
                 'created_at' => $post->created_at->toDateTimeString(),
