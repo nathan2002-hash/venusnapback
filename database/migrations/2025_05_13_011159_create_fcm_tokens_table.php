0a309776319f571a78db37747587082f0001cd6a
nathan2002-hash <78975906+nathan2002-hash@users.noreply.github.com>
2025-05-13 01:40:13 -0700

    Update 2025_05_13_011159_create_fcm_tokens_table.php

--- database/migrations/2025_05_13_011159_create_fcm_tokens_table.php
+++ database/migrations/2025_05_13_011159_create_fcm_tokens_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('token');
             $table->string('device_info');
+            $table->string('user_agent')->nullable();
             $table->string('status')->default('active');
             $table->timestamps();
         });
