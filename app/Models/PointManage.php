a01726775a19740b7242a9bcb66348c7f3ed99a6
Nathan Mwamba <nathanmwamba2002@gmail.com>
2025-05-16 15:26:51 -0700

    le

--- app/Models/PointManage.php
+++ app/Models/PointManage.php
@@ -6,6 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class PointManage extends Model
 {
+    protected $fillable = [
+        'user_id',
+        'manage_by',
+        'points',
+        'reason',
+        'type',
+        'status',
+        'metadata',
+        'balance_after',
+        'description',
+    ];
+
     public function user(){
         return $this->belongsTo(User::class);
     }
