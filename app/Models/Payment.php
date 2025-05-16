c2f2063e680f0238f9aeb655e8833a677ce51ac2
Your Name <you@example.com>
2025-05-16 04:12:55 -0700

    lost

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
-    protected $fillable = ['user_id', 'payment_session_id', 'amount', 'payment_method', 'currency', 'processor', 'payment_no', 'status', 'purpose', 'description'];
+    protected $fillable = ['user_id', 'payment_session_id', 'amount', 'payment_method', 'currency', 'processor', 'payment_no', 'status', 'purpose', 'metadata', 'description'];
 
     protected $casts = [
         'metadata' => 'array', // This automatically handles JSON encoding/decoding
