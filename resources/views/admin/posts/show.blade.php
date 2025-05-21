984cd9266448399315c70232f32053bbfb4ba08f
Your Name <you@example.com>
2025-05-21 02:12:31 -0700

    lok

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -153,12 +153,24 @@
             </ul>
 
             <div class="d-flex flex-wrap gap-5">
+                @if ($post->status == 'hold')
                 <button type="button" class="btn btn-primary waves-effect waves-light w-sm" data-bs-toggle="modal" data-bs-target="#hold">
+                    <i class="mdi mdi-pause d-block font-size-16"></i> UnHold
+                </button>
+                @else
+                    <button type="button" class="btn btn-primary waves-effect waves-light w-sm" data-bs-toggle="modal" data-bs-target="#hold">
                     <i class="mdi mdi-pause d-block font-size-16"></i> Hold
                 </button>
+                @endif
+               @if ($post->status == 'deletion')
+                <button type="button" class="btn btn-danger waves-effect waves-light w-sm" data-bs-toggle="modal" data-bs-target="#delete">
+                    <i class="mdi mdi-trash-can d-block font-size-16"></i> Reversal
+                </button>
+               @else
                 <button type="button" class="btn btn-danger waves-effect waves-light w-sm" data-bs-toggle="modal" data-bs-target="#delete">
                     <i class="mdi mdi-trash-can d-block font-size-16"></i> Delete
                 </button>
+               @endif
             </div>
         </div>
     </div>
