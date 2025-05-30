6ae554c9e91d1ae33afe9a2403c2ea736fd62f8c
Nathan Mwamba <nathanmwamba2002@gmail.com>
2025-05-30 00:50:21 -0700

    lost

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1215,6 +1215,21 @@
   <!-- Main JS File -->
   <script src="{{ asset('assets1/js/main.js') }}"></script>
 
+  <script>
+<script>
+    (function(d, w, c) {
+        w.BrevoConversationsID = '683948ffb662e85b0a0a95a4';
+        w[c] = w[c] || function() {
+            (w[c].q = w[c].q || []).push(arguments);
+        };
+        var s = d.createElement('script');
+        s.async = true;
+        s.src = 'https://conversations-widget.brevo.com/brevo-conversations.js';
+        if (d.head) d.head.appendChild(s);
+    })(document, window, 'BrevoConversations');
+</script>
+  </script>
+
 </body>
 
 </html>
