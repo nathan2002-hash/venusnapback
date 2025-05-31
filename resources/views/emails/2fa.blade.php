f88aa26cf7875b50ca8e7405ff7c294854f2af7c
Nathan Mwamba <nathanmwamba2002@gmail.com>
2025-05-31 07:05:32 -0700

    lost

--- resources/views/emails/2fa.blade.php
+++ resources/views/emails/2fa.blade.php
@@ -1,12 +1,13 @@
 <x-mail::message>
 # Hello {{ $name }},
 
+You're one snap away from shining brighter.
 Your Two-Factor Authentication (2FA) code is:
 
 # **{{ $code }}**
 
 This code is valid for 10 minutes. If you did not request this, please secure your account.
 
-Thanks,<br>
+Thank you for Keeping your account bright and secure.,<br>
 **Venusnap Security Team**
 </x-mail::message>
