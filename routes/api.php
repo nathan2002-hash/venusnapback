2a3976f10c1af7f92cf89dcc43dc912502df1073
Nathan Mwamba <nathanmwamba2002@gmail.com>
2025-06-17 03:34:33 -0700

    Los

--- routes/api.php
+++ routes/api.php
@@ -125,7 +125,7 @@ Route::middleware(['auth:api', 'check.account.status'])->group(function () {
     Route::get('/user/album/viewer/{albumId}', 'Api\AlbumController@showviewer');
     Route::post('/user/album/{id}/update-image', 'Api\AlbumController@album_update');
 
-    Route::get('/album/images', 'Api\AppStatusController@getAlbumImages');
+    Route::get('/api/album/images', 'Api\AppStatusController@getAlbumImages');
 
     Route::post('/user/album/delete/{id}', 'Api\AlbumAccessController@albumdelete');
 
