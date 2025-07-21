4e98051a658f432522e7b850f5f34cf76c8e826e
Nathan Mwamba <nathanmwamba2002@gmail.com>
2025-07-21 13:18:03 -0700

    lost

--- config/database.php
+++ config/database.php
@@ -172,6 +172,7 @@ return [
         ],
 
         'default' => [
+            'scheme' => env('REDIS_SCHEME', 'tcp'),
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
@@ -181,6 +182,7 @@ return [
         ],
 
         'cache' => [
+            'scheme' => env('REDIS_SCHEME', 'tcp'),
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
