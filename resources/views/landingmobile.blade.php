9212f415b4231a155d6393f0ebf264cf8bbf8322
Nathan Mwamba <nathanmwamba2002@gmail.com>
2025-09-25 07:25:06 -0700

    lost

--- resources/views/landingmobile.blade.php
+++ resources/views/landingmobile.blade.php
@@ -497,21 +497,21 @@
         // Sample art data
         const artData = [
             {
-                image: "https://images.unsplash.com/photo-1579546929662-711aa81148cf",
+                image: "{{ asset('assets/img/snap.png') }}",
                 artist: "SnapCrack",
                 title: "Addictive funny content",
                 description: "Addictive, fast, and funny. This album brings the internet’s funniest moments to life one meme at a time",
                 tags: ["Abstract", "Colorful", "Modern"]
             },
             {
-                image: "https://images.unsplash.com/photo-1579546929662-711aa81148cf",
+                image: "{{ asset('assets/img/snap.png') }}",
                 artist: "ToonPlay",
                 title: "Animated humor",
                 description: "ToonPlay is the home of animated humor where iconic characters, funny expressions.",
                 tags: ["Colorful", "Modern", "Expressionism"]
             },
             {
-                image: "https://images.unsplash.com/photo-1541961017774-22349e4a1262",
+                image: "{{ asset('assets/img/snap.png') }}",
                 artist: "Better Every Day",
                 title: "Source of inspiration",
                 description: "This album is your daily source of inspiration and practical wisdom to help you grow, heal, and thrive.",
