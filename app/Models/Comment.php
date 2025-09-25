88704f11b5acf9dfcadf70e450cf1e5313169436
Nathan Mwamba <nathanmwamba2002@gmail.com>
2025-09-25 03:55:59 -0700

    lost

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    protected $fillable = ['post_media_id', 'user_id', 'comment', 'status', 'gif_id', 'type', 'gif_url', 'gif_provider'];
+    protected $fillable = ['post_media_id', 'user_id', 'comment', 'status', 'gif_id', 'type', 'gif_url', 'gif_provider', 'comment_as_album_id', 'attachment_path', 'attachment_type'];
 
     public function postmedia(){
         return $this->belongsTo(PostMedia::class);
--- app/Models/CommentReply.php
+++ app/Models/CommentReply.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class CommentReply extends Model
 {
-    protected $fillable = ['comment_id', 'reply', 'status', 'gif_id', 'type', 'gif_url', 'gif_provider'];
+    protected $fillable = ['comment_id', 'reply', 'status', 'gif_id', 'type', 'gif_url', 'gif_provider', 'reply_as_album_id', 'attachment_path', 'attachment_type'];
 
     public function user(){
         return $this->belongsTo(User::class);
