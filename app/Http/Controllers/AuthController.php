4dcca0cf5046dcc0f6a1bfed26d7fb08a2c6addf
Nathan Mwamba <nathanmwamba2002@gmail.com>
2025-09-28 15:47:58 -0700

    lost

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -125,7 +125,7 @@ class AuthController extends Controller
         return response()->json([
             'status'  => 'success',
             'message' => 'Registration successful',
-            'redirect_url' => route('welcome') . '?name=' . urlencode($user->name)
+            'redirect_url' => '/onboard/welcome'
         ], 200);
     }
 
