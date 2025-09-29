2b5138242e8e1c5865e4efc4aaab076c7d2f4853
Nathan Mwamba <nathanmwamba2002@gmail.com>
2025-09-29 08:43:26 -0700

    lost

--- app/Http/Controllers/Api/AIController.php
+++ app/Http/Controllers/Api/AIController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Album;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Project;
 use App\Models\ChatMessage;
