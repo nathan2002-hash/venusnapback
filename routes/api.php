0e570c341648b54dab3598c796414a151b3b207d
Nathan Mwamba <nathanmwamba2002@gmail.com>
2025-10-05 13:52:42 -0700

    lost

--- routes/api.php
+++ routes/api.php
@@ -27,7 +27,7 @@ Route::middleware('auth:api')->group(function () {
     });
 });
 
-Route::post('/incoming/message/9s8df7as98df7asd', 'IncomingController@receive');
+Route::post('/incoming/message', 'IncomingController@recesive');
 Route::post('/webhook/vonage', 'Admin\MessageController@receive')->name('webhook.vonage');
 
 Route::post('/decrypt/filepath', function (Request $request) {
